1ac039efa04d3a621c45f6eeb5531ee58e2e6688
p-alex <alex.daniel9021@gmail.com>
2023-10-14 16:04:06 +0300

    Deleted OAuthList files

--- client/src/components/OAuthList/useOAuthListContext.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { OAuthListContext } from "./OAuthListContext";
-
-const useOAuthListContext = () => {
-  const oauthListContext = useContext(OAuthListContext);
-  return oauthListContext;
-};
-
-export default useOAuthListContext;
