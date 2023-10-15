1121c25469793dc6750e76b83f6325ff821b797f
p-alex <alex.daniel9021@gmail.com>
2023-10-15 18:04:57 +0300

    Removed deprecated function from schemas

--- client/src/schemas/user.schema.ts
+++ client/src/schemas/user.schema.ts
@@ -2,22 +2,18 @@ import { z } from "zod";
 
 export const emailSchema = z
   .string({ required_error: "Email is required" })
-  .nonempty("Email is required")
   .email("Email is invalid")
   .max(64, "Email must be at most 64 characters long");
 
 export const passwordSchema = z
   .string({ required_error: "Password is required" })
-  .nonempty("Password is required")
   .min(8, "Password must be at least 8 characters long")
   .regex(
     /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*/g,
     "Must contain at least 1 uppercase and lowercase letter, and 1 number",
   );
 
-export const confirmPasswordSchema = z
-  .string({ required_error: "Confirm password is required" })
-  .nonempty("Confirm password is required");
+export const confirmPasswordSchema = z.string({ required_error: "Confirm password is required" });
 
 export const signUpSchema = z
   .object({
