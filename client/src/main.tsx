790551733c392e5b3b941fbbfa878eb33780cb30
p-alex <alex.daniel9021@gmail.com>
2023-11-26 12:53:11 +0200

    Added @reduxjs/toolkit boilerplate code

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,15 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./authContext/AuthContextProvider.tsx";
+import { store } from "./app/store.ts";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
+    <Provider store={store}>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </Provider>
   </BrowserRouter>,
 );
